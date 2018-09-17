9b6c26b7b0cc1f50e7973fd7ad2699684f818a94
LadyS <ladysvideogame@gmail.com>
2018-09-17 10:28:14 +0200

    Creation of the project and Creation of the Titles Menu

---
+++ WGJ#62PlayTheVillain/Assets/Scripts/ScriptBtnQuit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptBtnQuit : MonoBehaviour {
+
+    private Button btnQuit;
+
+    // Use this for initialization
+    void Start () {
+
+        btnQuit = gameObject.GetComponent<Button>();
+        btnQuit.onClick.AddListener(Quit);
+
+
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+
+}
