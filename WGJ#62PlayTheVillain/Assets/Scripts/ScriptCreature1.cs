aacaaf656eb2a926c71aec776d8567b1de7e5008
LadyS <ladysvideogame@gmail.com>
2018-09-18 19:07:31 +0200

    Our creature1 detect ennemies and attack

--- WGJ#62PlayTheVillain/Assets/Scripts/ScriptCreature1.cs
+++ WGJ#62PlayTheVillain/Assets/Scripts/ScriptCreature1.cs
@@ -6,6 +6,13 @@ public class ScriptCreature1 : MonoBehaviour {
 
     [SerializeField]
     private float speed;
+
+    [SerializeField]
+    private int attackPoint;
+
+    [SerializeField]
+    private int attackSpeed;
+
     private float step;
     
     private ArrayList path;
@@ -14,6 +21,9 @@ public class ScriptCreature1 : MonoBehaviour {
     private int iteration;
 
     private float timer;
+
+    private bool isInCombat;
+    private GameObject currentEnnemy;
 
 	// Use this for initialization
 	void Start () {
@@ -32,22 +42,58 @@ public class ScriptCreature1 : MonoBehaviour {
 
         timer = 0;
 
+        isInCombat = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-                
-        if (iteration < path.Count && gameObject.transform.position == currentPosition) {
 
-            currentPosition = (Vector3)path[iteration];
-            
-            iteration = iteration + 1;
-                       
+        //ennemy detection
+        Vector2 rayStart = new Vector2(gameObject.transform.position.x + 0.5f, gameObject.transform.position.y - 0.55f);
+        RaycastHit2D ray = Physics2D.Raycast(rayStart, -Vector2.up);
+        if (ray.collider != null)
+        {
+            //Debug.DrawRay(rayStart, -Vector2.up);
+            isInCombat = true;
+            currentEnnemy = ray.collider.gameObject;
         }
 
-        step = speed * Time.deltaTime;
+        //translation  
+        if (isInCombat == false)
+        {
+            if (iteration < path.Count && gameObject.transform.position == currentPosition)
+            {
 
-        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, currentPosition, step);
-        
+                currentPosition = (Vector3)path[iteration];
+
+                iteration = iteration + 1;
+
+            }
+
+            step = speed * Time.deltaTime;
+
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, currentPosition, step);
+        }
+        else
+        {
+            timer = timer + Time.deltaTime;
+
+            if (timer >= attackSpeed && currentEnnemy.GetComponent<ScriptEnnemy1>().GetHealth() > 0)
+            {
+
+                currentEnnemy.GetComponent<ScriptEnnemy1>().SetHealth(currentEnnemy.GetComponent<ScriptEnnemy1>().GetHealth() - attackPoint);
+
+                if ((currentEnnemy != null && currentEnnemy.GetComponent<ScriptEnnemy1>().GetHealth() <= 0) || currentEnnemy == null)
+                {
+                    isInCombat = false;
+                }
+
+                timer = 0;
+
+            }
+
+        }
+
     }
 }
---
+++ WGJ#62PlayTheVillain/Assets/Scripts/ScriptEnnemy1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptEnnemy1 : MonoBehaviour {
+
+    [SerializeField]
+    private int health;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    //Health Getter
+    public void SetHealth(int health)
+    {
+        this.health = health;
+    }
+
+    //Health Setter
+    public int GetHealth()
+    {
+        return health;
+    }
+
+}
