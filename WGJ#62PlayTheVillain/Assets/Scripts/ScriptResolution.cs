5ee85c457c3d2ef34c22dafc8616c3d3177383a4
LadyS <ladysvideogame@gmail.com>
2018-09-24 10:03:30 +0200

    Final Build

---
+++ WGJ#62PlayTheVillain/Assets/Scripts/ScriptResolution.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptResolution : MonoBehaviour {
+
+    private void Awake()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
